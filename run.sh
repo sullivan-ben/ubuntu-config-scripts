724ece495cc29bfd67c3dbc52ec3e08d21f1d81a
Ben Sullivan <b.sullivan598@gmail.com>
2023-01-05 15:04:32 +1100

    Added formatting to message output

--- run.sh
+++ run.sh
@@ -3,14 +3,20 @@
 OUTPUT_FILE=/tmp/config-log.txt
 
 echo "Running setup..." >> $OUTPUT_FILE
+echo "\n" >> $OUTPUT_FILE
 
 sudo ./general/ubuntu-server.sh
 sudo ./vim/ubuntu-server-vim.sh
 sudo ./zsh/ubuntu-zsh-generic.sh
 
 sudo ./ssh/config-bitbucket.sh $OUTPUT_FILE
+echo "\n" >> $OUTPUT_FILE
+
 sudo ./ssh/config-github.sh $OUTPUT_FILE
+echo "\n" >> $OUTPUT_FILE
+
 sudo ./ssh/config-gitlab.sh $OUTPUT_FILE
+echo "\n" >> $OUTPUT_FILE
 
 sudo ./ssh/config-lan-known-hosts.sh
 
