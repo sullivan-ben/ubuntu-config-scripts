86fe860e91ef4bf16c5bde028443edc9f2b5ae1e
Ben Sullivan <b.sullivan598@gmail.com>
2023-01-05 13:18:21 +1100

    Added output messages bookending run script

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 OUTPUT_FILE=/tmp/config-log.txt
 
+echo "Running setup..." >> $OUTPUT_FILE
+
 sudo ./general/ubuntu-server.sh
 sudo ./vim/ubuntu-server-vim.sh
 sudo ./zsh/ubuntu-zsh-generic.sh
@@ -11,6 +13,8 @@ sudo ./ssh/config-github.sh $OUTPUT_FILE
 sudo ./ssh/config-gitlab.sh $OUTPUT_FILE
 
 sudo ./ssh/config-lan-known-hosts.sh
+
+echo "Setup complete" >> $OUTPUT_FILE
 
 cat $OUTPUT_FILE
 rm $OUTPUT_FILE
